d163ddab2c3c339f29e076c0b12816552141e31b
lucindalin000 <lin.ting014@gmail.com>
2025-02-28 14:52:24 -0500

    Update identity.ts-AI name
    add 😋

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `FOODIE Luci`;
 export const OWNER_DESCRIPTION: string = `A passionate advocate for local restaurants, sharing insider tips and creative dining ideas with a personal touch`;
 
-export const AI_NAME: string = `FoodieBuddy`;
+export const AI_NAME: string = `FoodieBuddy😋`;
 export const AI_TONE: string = `Energetic & Enthusiastic`;
 export const AI_ROLE: string = `Dining Companion`;
